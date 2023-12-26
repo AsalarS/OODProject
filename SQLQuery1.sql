dcc252ece7fbebd64361cfeca500e842636df392
Danial6060 <danial6060@gmail.com>
2023-12-26 09:36:10 +0300

    don

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO Feedback (FeedbackContent, CourseID, StudentID) VALUES ('Great course!', 1, 1);
+INSERT INTO Feedback (FeedbackContent, CourseID, StudentID) VALUES ('Good teacher!', 2, 2);
+INSERT INTO Feedback (FeedbackContent, CourseID, StudentID) VALUES ('Interesting material!', 3, 1);
+INSERT INTO Feedback (FeedbackContent, CourseID, StudentID) VALUES ('Very helpful!', 4, 1);
+INSERT INTO Feedback (FeedbackContent, CourseID, StudentID) VALUES ('Excellent course!', 5, 3);
